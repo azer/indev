6cd6cc91d02582f07903e8722be16c25441bc5a4
E.Azer Koçulu <azer@kodfabrik.com>
2013-07-18 23:41:23 -0700

    termlist

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function start(argv){
   process.stdout.write('\u001B[2J\u001B[0;0f');
 
   tasks.specified = argv._;
-  tasks.specified.length == 0 && tasks.specified.push('all');
 
   if(argv.file) lookup.splice(0, 0, argv.file);
 
